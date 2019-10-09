234955830cf1dd80716f0eb6583976469745845c
SeonCheol Kim <ksc4216@gmail.com>
2019-10-09 09:02:15 +0900

    Create Single Number_136.js

---
+++ leetcode/Single Number_136.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let result = 0
+    
+    nums.forEach( num => result ^= num)
+    return result
+};
